44f9233f7204dc47eef276815d1061da776fafb8
ina357 <nature10017@naver.com>
2022-03-03 10:47:48 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const $container=$('.gallery');     //변하지않는애 이미지들을 감사는 갤러리
+const $loadMoreBtn=$('.load-more');     //변하지않는애 더보기버튼
+let $addItemCount=8;     //누를떄마다 추가됐으면좋겠다 하는 숫자를 써넣는다
+let $addde=0;     //대입연산자:처음보이는 것 리스트 항목 모두 로드하면 버튼이 사라지게 할 용도
+let $allData=[]; //배열구조
+
+//$.getJSON('파일경로',할일);
+$.getJSON('./data/content.json',function(data){ //데이타라는 매개 변수를 준다.
+
+
+});
+function initGallery(data){
+    $allData=data;
+    console.log($allData)
+}
